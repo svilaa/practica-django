044350834f2a3c4402c2f7bd00dc643269c0a6b2
Sergi <svilaalmenara@gmail.com>
2014-03-18 00:21:16 +0100

    Canvi de path absolut a path relatiu

--- sobres/sobres/settings.py
+++ sobres/sobres/settings.py
@@ -113,7 +113,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-	'/home/user06/djangostack-1.5.5-2/practica-django/sobres/isobres/mainpage.html',
+	os.path.join(os.path.dirname(__file__), '..isobres/templates'),
 )
 
 INSTALLED_APPS = (
