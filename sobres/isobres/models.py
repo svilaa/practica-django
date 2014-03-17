3c292277df8511f3b1a4c689be096081e3c1afee
Sergi <svilaalmenara@gmail.com>
2014-03-17 11:16:04 +0100

    Actualitzacio

---
+++ sobres/isobres/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Sobre(models.Model):
+	date = models.DateTimeField()
+	amount = models.IntegerField()
+	concept = models.TextField(max_length=100)
+
+class Donor(models.Model):
+	name = models.CharField(max_length=40)
+
+class Sobre(models.Model):
+	date = models.DateTimeField()
+	amount = models.IntegerField()
+	concept = models.TextField(max_length=100)
+	donor = models.ForeignKey(Donor)
+	user = models.ForeignKey(User)
